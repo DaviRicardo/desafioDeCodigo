b56bd4d9b3b5138caf5b80212b8531a5e5126ec6
Davi Silva <daviricardo205@gmail.com>
2023-07-05 23:49:32 -0300

    Resolucao do exercicio 1

---
+++ src/Ex1.java
@@ -0,0 +1,19 @@
+/*
+1. Reverta a ordem das palavras nas frases, mantendo a ordem das palavras:
+a. Input: "Hello, World! OpenAl is amazing."
+b. Output: "amazing. is OpenAI World! Hello,"
+*/
+public class Ex1 {
+    public Ex1() {
+
+    }
+    public String inverterPalavras(String frase) {
+        String[] palavras = frase.split(" ");
+        StringBuilder fraseInvertida = new StringBuilder();
+
+        for (int i = palavras.length - 1; i >= 0; i--) {
+            fraseInvertida.append(palavras[i]).append(" ");
+        }
+        return fraseInvertida.toString().trim();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,19 @@
+import javax.swing.*;
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        int opcaoMenu;
+        String input;
 
+        opcaoMenu = Integer.parseInt(JOptionPane.showInputDialog(
+                null, "1 - Reverter String", "Escolha uma opção: ", JOptionPane.INFORMATION_MESSAGE));
+
+        switch (opcaoMenu) {
+            case 1:
+                Ex1 exercicio1 = new Ex1();
+                input = JOptionPane.showInputDialog("Digite uma frase: ");
+                JOptionPane.showMessageDialog(null, exercicio1.inverterPalavras(input), "Frase invertida", JOptionPane.INFORMATION_MESSAGE);
+                break;
+        }
+    }
 }
